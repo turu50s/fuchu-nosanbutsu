fce507d4853b47068071610faf16ca518867b93f
tsuruki <sigeru0216@gmail.com>
2019-07-15 08:14:02 +0900

    Add ignore files

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BulkImport
+module FuchuNosanbutsu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
