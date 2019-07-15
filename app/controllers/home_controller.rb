069bd8d9181805ef6385e6bee182ed5353fc662b
tsuruki <sigeru0216@gmail.com>
2019-07-15 14:07:29 +0900

    検索パラメータの取得の部分を修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @q = params[:q][:description_cont]
+    @q = params[:q].nil? ? '' : params[:q][:description_cont]
     @search = Store.ransack(params[:q])
     @stores = @search.result.page(params[:page]).per(10)
   end
