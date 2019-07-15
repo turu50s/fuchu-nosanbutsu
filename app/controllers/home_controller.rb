d81d8ec46b5b86bb1763e54be604dbcbada1d545
tsuruki <sigeru0216@gmail.com>
2019-07-15 10:32:34 +0900

    キーワードをハイライト表示する

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @q = params[:q][:description_cont]
     @search = Store.ransack(params[:q])
     @stores = @search.result.page(params[:page]).per(10)
   end
