78cd0e4e98629f83acaefe3dac2e9849be4bc320
tsuruki <sigeru0216@gmail.com>
2019-07-15 21:33:53 +0900

    登録、更新、削除を実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,36 @@ class HomeController < ApplicationController
     @search = Store.ransack(params[:q])
     @stores = @search.result.page(params[:page]).per(10)
   end
+
+  def new
+    @store = Store.new
+  end
+  
+  def create
+    @store = Store.new(store_params)
+    @store.save!
+    redirect_to home_index_path, notice: "新規データを登録しました"
+  end
+
+  def edit
+    @store = Store.find(params[:id])
+  end
+  
+  def update
+    store = Store.find(params[:id])
+    store.update!(store_params)
+    redirect_to home_index_path, notice: "データを更新しました"
+  end
+
+  def destroy
+    store = Store.find(params[:id])
+    store.destroy
+    redirect_to home_index_path, notice: "データを削除しました"
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :address, :description)
+  end
 end
